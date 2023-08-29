37f824be766661b56dfabb9b1fec03b2ec54fab0
bigshu13 <bigshu13@gmail.com>
2023-08-28 21:27:23 -0400

    updated models and added order model

--- models/user.js
+++ models/user.js
@@ -3,22 +3,31 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema; 
 
+const SALT_ROUNDS = 5;
 //user schema will need name (string) require this to be true and email (string) require to be true, and make it unique, so emails cant be the same
 //password(string) add mininum length and required
 //add timestamps to know when people sign in/up
+//add isLoggedin boolean and trim to user schema
 const userSchema = new Schema ({
     username: {type: String, required: true},
-    email: {type: String, unique: true, required: true},
-    password: {type: String, minLength: 5, required: true}
+    email: {type: String, unique: true, trim: true, required: true},
+    password: {type: String, minLength: 5, trim: true, required: true},
+    isLoggedIn: {type: Boolean, required: false}
 }, {
-timestamps: true
-})
+timestamps: true,
+toJSON: {
+    transform: function(doc, ret) {
+        delete ret.password;
+        return ret;
+        }
+    }
+});
 
 //add userschema save function
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     //updates the password with the computed hash
-    this.password = await bcrypt.hash(this.password);
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
     return next();
 })
 
