4f8f026153f1cb177f2c195662fca9995c67fa1f
Jorgetorreslopez <usda4ever@gmail.com>
2023-08-29 22:20:22 -0400

    Fixed ItemSchema

--- models/order.js
+++ models/order.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const itemSchema = require('./itemSchema');
-const item = require('./item');
 
 const lineItemSchema = new Schema ({
     quantity: {type: Number, default: 1},
