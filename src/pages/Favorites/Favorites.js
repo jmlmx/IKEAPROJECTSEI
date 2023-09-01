b47f3674cd4039d58fa6ff07c50da666bff074f2
Tstreets1000 <terrance.jdhill@yahoo.com>
2023-08-31 23:00:50 -0400

    Added Pages

---
+++ src/pages/Favorites/Favorites.js
@@ -0,0 +1,12 @@
+//==== NEEDED COMPONENTS FOR FAVORITES SCREEN ====//
+    /*
+    Logo
+    USER PORTAL COMPONENT
+    FOOTER COMPONENT
+    NavBar
+
+    -  Using same functionality as order history: Items will have "is liked boolean" defalted to false.
+        Only becomes true when liked checkbox under item is clicked. (Checkbox created in MenuListItem)
+        Ternary: If checkbox is checked, push top array of objects named favorites.
+    
+    */
