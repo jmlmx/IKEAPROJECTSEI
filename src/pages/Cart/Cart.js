11fe6e1d8e076d555d2d23440ff34798b2b02341
Jorgetorreslopez <usda4ever@gmail.com>
2023-09-09 18:33:52 -0400

    Styled Cart Prototype

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -2,59 +2,53 @@ import LineItem from '../../components/LineItem/LineItem';
 import { useNavigate } from 'react-router-dom';
 import styles from './Cart.module.scss';
 
-export default function Cart({ cart, handleChangeQty, toggleShowCart, user }) {
-	console.log(user)
-	if (!cart) return null;
+export default function Cart({ cart, handleChangeQty, user }) {
+  if (!cart) return null;
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  const lineItems = cart.lineItems.map((item) => (
+    <LineItem
+      lineItem={item}
+      isPaid={cart.isPaid}
+      handleChangeQty={handleChangeQty}
+      key={item._id}
+    />
+  ));
 
-	const lineItems = cart.lineItems.map((item) => (
-		<LineItem
-			lineItem={item}
-			isPaid={cart.isPaid}
-			handleChangeQty={handleChangeQty}
-			key={item._id}
-		/>
-	));
+  function handleCheckoutClick() {
+    navigate('/checkout');
+  }
 
-	function handleCheckoutClick() {
-		toggleShowCart();
-		navigate('/checkout');
-	}
-
-	return (
-		<div className={styles.CartBackground}>
-			<div className={styles.CartPanel}>
-				<div className={styles.cartHeading}>
-					<span className={styles.itemQty}>{`${cart.totalQty} item${
-						cart.totalQty !== 1 ? 's' : ''
-					}`}</span>
-					<button className={styles.closeBtn} onClick={toggleShowCart}>
-						close
-					</button>
-				</div>
-				<div className={`${styles.lineItemContainer} scroll-y`}>
-					{lineItems.length ? (
-						<>
-							{lineItems}
-							<section className={styles.total}>
-								<span>${cart.orderTotal.toFixed(2)}</span>
-								{!cart.isPaid && (
-									<button
-										className={styles.checkoutBtn}
-										onClick={handleCheckoutClick}
-										disabled={!lineItems.length}
-									>
-										checkout
-									</button>
-								)}
-							</section>
-						</>
-					) : (
-						<div className={styles.empty}>your cart is empty.</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles['cart-container']}>
+      <div>
+        <div className={styles['cart-header']}>
+          <span className={styles['item-count']}>{`${cart.totalQty} item${
+            cart.totalQty !== 1 ? 's' : ''
+          }`}</span>
+        </div>
+        <div className={`${styles['line-items']} scroll-y`}>
+          {lineItems.length ? (
+            <>
+              {lineItems}
+              <section className={styles['total-section']}>
+                <span>${cart.orderTotal.toFixed(2)}</span>
+                {!cart.isPaid && (
+                  <button
+                    className={styles['checkout-btn']}
+                    onClick={handleCheckoutClick}
+                    disabled={!lineItems.length}
+                  >
+                    checkout
+                  </button>
+                )}
+              </section>
+            </>
+          ) : (
+            <div className={styles['empty-cart']}>your cart is empty.</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
