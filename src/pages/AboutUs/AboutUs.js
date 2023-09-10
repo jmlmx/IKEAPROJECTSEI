f210a13897771b511d8192eca5f0d33e18ccb2ea
jcarlson <jake.carlson90@gmail.com>
2023-09-10 12:44:15 -0700

    made AboutUs.js file with terrance and updated app.js with new route

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,13 +1,65 @@
-//==== NEEDED COMPONENTS FOR ABOUTUS SCREEN ====//
-    /*
-    Logo
-    USER PORTAL 
-    FOOTER COMPONENT
-    NavBar
+import React from 'react';
 
-    - PICTURES OF TEAM MEMBERS
-            * TEAM MEMBER MODAL W/ STATS (POP-UP STAT SHEET)
-    - STATEMENT ABOUT WEBSITE
-    - MISSION STATEMENT - (use PowerPoint Statement)
-    */
+const members = [
+        'Terrance Hill',
+        'Jorge Lopez',
+        'Jamal Mayon',
+        'Shumar Mcpherson',
+        'Jake Carlson',
+        'Diego Vasquez',
+        'Yami Narvaez'
+]
+
+export default function AboutUs({ members }) {
+
+        return (
+                <main>
+                        <div>
+                                <p>
+                                        Mission Statement: At ShuKea, our mission is to transform houses into homes by providing high-quality, stylish, and functional furniture that enriches the lives of our customers. We are dedicated to offering an exceptional shopping experience, where craftsmanship,  affordability, and sustainability converge.
+                                </p>
+                                <p>
+                                        Website Statement: 
+                                </p>
+                        </div>
+                        <div>
+                                <p>
+                                        Terrance Hill - Scrum Master
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Jorge Lopez - GitHub Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Jamal Mayon - User Services Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Shumar McPherson - Database Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Jake Carlson - User Services Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Diego Vasquez - Front End Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                                <p>
+                                        Yami Navaez - Styling Manager
+                                        <img>Picture of team member</img>
+                                </p>
+                        </div>
+                </main>
+        )
+}
+
+
+/*
+- PICTURES OF TEAM MEMBERS
+- STATEMENT ABOUT WEBSITE
+- MISSION STATEMENT - (use PowerPoint Statement)
+*/
 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -18,7 +18,8 @@ import Footer from '../../components/Footer/Footer';
 import UserPortal from '../../components/UserPortal/UserPortal';
 import NavBar from '../../components/NavBar/NavBar';
 import AuthPage from '../../pages/AuthPage/AuthPage';
-import Favorites from '../../pages/Favorites/Favorites'
+import Favorites from '../../pages/Favorites/Favorites';
+import AboutUs from '../../pages/AboutUs/AboutUs'
 
 export default function App() {
 	const [pexelsData, setPexelsData] = useState([]);
@@ -100,6 +101,14 @@ export default function App() {
 							setUser={setUser}
 							cart={cart}
 							setCart={setCart}
+						/>
+					}
+				/>
+				<Route
+					path="/AboutUs"
+					element={
+						<AboutUs
+							members={members}
 						/>
 					}
 				/>
