19991491db29c87deb3f3697f0eb7d4e4b777cbc
jmlmx <jmayon0@gmail.com>
2023-09-11 22:28:07 -0400

    Possible Improvements To User Faves

--- src/components/FavoriteList/FavoriteList.js
+++ src/components/FavoriteList/FavoriteList.js
@@ -1,21 +1,25 @@
 import MenuListItem from '../MenuListItem/MenuListItem';
 import styles from './FavoriteList.module.scss'
 export default function FavoriteList({ favorites, handleAddToOrder, user, setUser }) {
+  
   console.log(user)
-    const favoriteItems = user.favorites.map(fave =>
+  
+  const isUserLoggedIn = user.isLoggedIn
+  
+  const userHasFaves = user.favorites.length > 0
+
+    const favoriteItems = user.favorites.map((fave) =>
       <MenuListItem
       key={fave._id}
       handleAddToOrder={handleAddToOrder}
       favoriteItem={fave}
       />
     );
-    const isLoggedIn = user.isLoggedIn
-    const hasFaves = user.favorites.length > 0
     // Link favorites to logged in user here?
     return (
       <main className={styles.FavoriteList}>
-        {isLoggedIn ? (
-          hasFaves ? (
+        {isUserLoggedIn ? (
+          userHasFaves ? (
             {favoriteItems}
         ):(
             <span className={styles.noFavorites}>You Have No Favorites</span>
